aac2c2ae67bc68587e5baa326b84554003aa9c49
VIKASH KUMAR <vikashjjp728@gmail.com>
2023-07-02 13:08:46 +0530

    portfolio

--- public/js/resume.js
+++ public/js/resume.js
@@ -6,7 +6,7 @@ year.innerHTML = date.getFullYear();
 //header properties
 let logoImage=document.getElementById('logo-img');
 let logoText=document.getElementById('logo-text');
-let logourl="/";
+let logourl="https://vikashkrdeveloper.github.io/portfolio.github.io/";
 logoImage.addEventListener('click',()=>{
     window.location.assign(logourl);
 })
@@ -20,7 +20,7 @@ let projects = document.getElementById('project');
 let education = document.getElementById('education');
 let contact = document.getElementById('contact');
 const assignurl=()=>{
-    let url="/";
+    let url="https://vikashkrdeveloper.github.io/portfolio.github.io/";
     window.location.assign(url);
 
 
